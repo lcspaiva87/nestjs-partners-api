3275bce12f5f6cea26aed531180fc821947c79d5
paiva <lcspaiva87@gmail.com>
2024-06-22 13:58:17 -0300

    feat: Extend PrismaService to connect to the database on module initialization

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
-
+import { Global, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+@Global()
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
