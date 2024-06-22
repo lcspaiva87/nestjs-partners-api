a5ce5aad02f65c21aec9f32c0a3ee51577935fe5
paiva <lcspaiva87@gmail.com>
2024-06-22 13:54:49 -0300

    refactor: Add Prisma module to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventController } from './event/event.controller';
 import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, PrismaModule],
   controllers: [AppController, EventController],
   providers: [AppService],
 })
